03828e70f81c1d344a5a6cf43301ea62481bba5c
Adithyan Binu <adithyanbinu666@gmail.com>
2025-05-26 19:46:19 +0530

    Time: 20 ms (21.56%), Space: 62.5 MB (57.64%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.js
@@ -0,0 +1,39 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+var canFinish = function (numCourses, prerequisites) {
+
+    let graph = new Map()
+
+    for (let [a, b] of prerequisites) {
+        if (!graph.has(b)) {
+            graph.set(b, [])
+        }
+        graph.get(b).push(a)
+    }
+
+    let arr = new Array(numCourses).fill(0)
+
+    function dfs(course) {
+        if (arr[course] == 1) return false
+        if (arr[course] == 2) return true
+
+        arr[course] = 1
+        for (let neigh of graph.get(course) || []) {
+            if (!dfs(neigh)) return false
+        }
+        arr[course] = 2
+        return true
+    }
+
+    for (let i = 0; i < numCourses; i++) {
+        if (!dfs(i)) return false
+    }
+
+return true
+
+
+
+};
