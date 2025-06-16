4878a0c2653b98ed9a7a8e38b07e1e40f9922a24
Adithyan Binu <adithyanbinu666@gmail.com>
2025-06-16 13:16:04 +0530

    Time: 0 ms (100%), Space: 53.9 MB (69.29%) - LeetHub

--- 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.js
+++ 2144-maximum-difference-between-increasing-elements/2144-maximum-difference-between-increasing-elements.js
@@ -4,19 +4,33 @@
  */
 var maximumDifference = function (nums) {
 
-    let diff = 0
+    // let diff = 0
 
-    for (let i = 0; i < nums.length - 1; i++) {
-        for (let j = i + 1; j < nums.length; j++) {
-            if (nums[i] < nums[j] && Math.abs(nums[j] - nums[i]) > diff) {
-                diff = Math.abs(nums[j] - nums[i])
-            }
+    // for (let i = 0; i < nums.length - 1; i++) {
+    //     for (let j = i + 1; j < nums.length; j++) {
+    //         if (nums[i] < nums[j] && Math.abs(nums[j] - nums[i]) > diff) {
+    //             diff = Math.abs(nums[j] - nums[i])
+    //         }
+    //     }
+    // }
+
+    // if(diff == 0){
+    //     return -1
+    // }else{
+    //     return diff
+    // }
+
+    let min = nums[0]
+    let diff = -1
+
+    for(let i =1 ; i< nums.length ; i++){
+        if(nums[i]>min){
+            diff = Math.max(diff, nums[i]- min)
+        }else{
+            min = nums[i]
         }
     }
+    return diff
 
-    if(diff == 0){
-        return -1
-    }else{
-        return diff
-    }
+
 };
