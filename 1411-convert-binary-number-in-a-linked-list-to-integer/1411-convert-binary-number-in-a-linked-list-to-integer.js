53700d9ae6f79b8b1c28ed7413b50276662ee884
Adithyan Binu <adithyanbinu666@gmail.com>
2025-07-14 12:31:07 +0530

    Time: 2 ms (4.35%), Space: 53.8 MB (36.02%) - LeetHub

--- 1411-convert-binary-number-in-a-linked-list-to-integer/1411-convert-binary-number-in-a-linked-list-to-integer.js
+++ 1411-convert-binary-number-in-a-linked-list-to-integer/1411-convert-binary-number-in-a-linked-list-to-integer.js
@@ -9,15 +9,26 @@
  * @param {ListNode} head
  * @return {number}
  */
-var getDecimalValue = function(head) {
+var getDecimalValue = function (head) {
 
     let curr = head
     let binary = ""
-    
-    while (curr){
-        binary+=curr.val;
+
+    while (curr) {
+        binary += curr.val;
         curr = curr.next
     }
 
-    return parseInt(binary,2)
+    let convert = (str) => {
+        let rev = str.split("").reverse().join("")
+        console.log(rev)
+        let num = 0
+        for (let i = 0; i < rev.length; i++) {
+            num += (Number(rev[i] * Math.pow(2,i)))
+        }
+        return num
+    }
+
+
+    return convert(binary)
 };
